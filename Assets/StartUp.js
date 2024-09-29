6e6e095d9c42e7ea0bb1fd7c4055e7790e38a4f6
richardzhang32@gmail.com <richardzhang32@gmail.com>
2024-09-29 05:32:03 -0400

    fix

--- Assets/StartUp.js
+++ Assets/StartUp.js
@@ -12,8 +12,8 @@ function instantiatePellets() {
     var startPosition = new vec3(0, -5, 0); // Adjust this as necessary
 
     for (let i = 0; i < NUM_SPHERES; i++) {
-        var pellet = script.pelletPrefab.instantiate(script.getSceneObject());
-        var position = new vec3(startPosition.x, startPosition.y, startPosition.z + (i * 100) * -1);
+        var pellet = script.pelletPrefab.instantiate(script.startScreen.getParent());
+        var position = new vec3(startPosition.x, startPosition.y, -100 + stawrtPosition.z + (i * 100) * -1);
         pellet.getTransform().setWorldPosition(position);
     }
     print("Pellets Created");
